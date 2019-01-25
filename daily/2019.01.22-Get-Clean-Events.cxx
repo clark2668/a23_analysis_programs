b024315945a2e75f74cddaa1845da96f993a50b7
clark2668 <clark.2668@osu.edu>
2019-01-25 16:41:36 -0500

    Add program for Jorge that checks for if an event is CW clean

---
+++ daily/2019.01.22-Get-Clean-Events.cxx
@@ -0,0 +1,151 @@
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////	2019.01.22-Get-Clean-Events.cxx 
+////	Check an event for CW contamination
+////
+////	Jan 2019
+////////////////////////////////////////////////////////////////////////////////
+
+//C++
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <math.h>
+
+//ROOT Includes
+#include "TTree.h"
+#include "TFile.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+	stringstream ss;
+	
+	if(argc<3){
+		cout<< "Usage\n" << argv[0] << " <station> <year> <run_file_name 1> <run_file_name 2> ..."<<endl;
+		return 0;
+	}
+	int station = atoi(argv[1]);
+	int year = atoi(argv[2]);
+
+	for(int file_num=3; file_num<argc; file_num++){
+
+		string chRun = "run";
+		string file = string(argv[file_num]);
+		size_t foundRun = file.find(chRun);
+		string strRunNum = file.substr(foundRun+4,4);
+		int runNum = atoi(strRunNum.c_str());
+		printf("Run Number %d \n", runNum);
+
+		cout << "Run " << file_num << " :: " << argv[file_num] << endl;
+
+		//we need to open the CW file
+
+		char summary_file_name[400];
+		sprintf(summary_file_name,"/fs/scratch/PAS0654/ara/10pct/CWID/A%d/%d/CWID_station_%d_run_%d.root",station,year,station,runNum);
+		TFile *NewCWFile = TFile::Open(summary_file_name);
+		if(!NewCWFile) {
+			std::cerr << "Can't open new CW file\n";
+			return -1;
+		}
+		TTree* NewCWTree = (TTree*) NewCWFile->Get("NewCWTree");   
+		if(!NewCWTree) {
+			std::cerr << "Can't find NewCWTree\n";
+			return -1;
+		}
+		vector<vector<double> > *badFreqs_fwd =0;
+		vector<vector<double> > *badFreqs_back=0;
+		vector<vector<double> > *badSigmas_fwd=0;
+		vector<vector<double> > *badSigmas_back=0;
+		vector<vector<double> > *badFreqs_baseline=0;
+
+		// inside, there are five trees
+		// 2 x bad frequencies identified with the Carl's phase variance (forward and backwards running)
+		// 2 x how bad they are (the sigmas)
+		// 1 x bad frequencies identified with Eugene's "baseline violation" technique from Testbed
+
+		NewCWTree->SetBranchAddress("badFreqs_fwd",&badFreqs_fwd);
+		NewCWTree->SetBranchAddress("badSigmas_fwd",&badSigmas_fwd);
+		NewCWTree->SetBranchAddress("badFreqs_back",&badFreqs_back);
+		NewCWTree->SetBranchAddress("badSigmas_back",&badSigmas_back);
+		NewCWTree->SetBranchAddress("badFreqs_baseline",&badFreqs_baseline);
+
+		int numEntries = NewCWTree->GetEntries();
+		Long64_t starEvery=numEntries/200;
+		if(starEvery==0) starEvery++;
+
+		//now to loop over events
+		for(int event=0; event<numEntries; event++){
+
+			NewCWTree->GetEntry(event);
+
+			bool isCutonCW_fwd[2]; isCutonCW_fwd[0]=false; isCutonCW_fwd[1]=false;
+			bool isCutonCW_back[2]; isCutonCW_back[0]=false; isCutonCW_back[1]=false;
+			bool isCutonCW_baseline[2]; isCutonCW_baseline[0]=false; isCutonCW_baseline[1]=false;
+			
+			//first, check the baseline technique
+			for(int pol=0; pol<badFreqs_baseline->size(); pol++){
+				vector<double> badFreqListLocal_baseline = badFreqs_baseline->at(pol);
+				if(badFreqListLocal_baseline.size()>0) isCutonCW_baseline[pol]=true;
+			}
+
+			//second, check the "forward" looking phase variance list
+			double threshCW = 1.0;
+			vector<double> badFreqList_fwd;
+			vector<double> badSigmaList_fwd;
+			for(int pol=0; pol<badFreqs_fwd->size(); pol++){
+				badFreqList_fwd=badFreqs_fwd->at(pol);
+				badSigmaList_fwd=badSigmas_fwd->at(pol);
+				for(int iCW=0; iCW<badFreqList_fwd.size(); iCW++){
+					if(
+						badSigmaList_fwd[iCW] > threshCW 
+						&& 
+						abs(300. - badFreqList_fwd[iCW]) > 2.
+						&&
+						abs(500. - badFreqList_fwd[iCW]) > 2.
+						&&
+						abs(125. - badFreqList_fwd[iCW]) > 2.
+					){
+						isCutonCW_fwd[pol] = true;
+					}
+				}
+			}
+
+			//third (and finally), check the "backwards" looking phase variance list
+			vector<double> badFreqList_back;
+			vector<double> badSigmaList_back;
+			for(int pol=0; pol<badFreqs_back->size(); pol++){
+				badFreqList_back=badFreqs_back->at(pol);
+				badSigmaList_back=badSigmas_back->at(pol);
+				for(int iCW=0; iCW<badFreqList_back.size(); iCW++){
+					if(
+						badSigmaList_back[iCW] > threshCW 
+						&& 
+						abs(300. - badFreqList_back[iCW]) > 2.
+						&&
+						abs(500. - badFreqList_back[iCW]) > 2.
+						&&
+						abs(125. - badFreqList_back[iCW]) > 2.
+					){
+						isCutonCW_back[pol] = true;
+					}
+				}
+			}
+
+			for(int pol=0; pol<2; pol++){
+
+				//if it's not contaminated by *any* CW, do whatever you want
+				if(!isCutonCW_fwd[pol] && !isCutonCW_back[pol] && !isCutonCW_baseline[pol]){
+					PlotThisEvent(station,year,runNum,event, settings, detector, theCorrelators);
+				}
+			
+			} //cal pulser
+		
+		} //loop over events
+		NewCWFile->Close();
+		delete NewCWFile;
+	} //end loop over input files
+}
