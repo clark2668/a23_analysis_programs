2a0755c58c981c2548238a23cec5c0cb5bbfc706
clark2668 <clark.2668@osu.edu>
2019-02-22 08:49:47 -0500

    Using wrong argv for file name

--- examples/example_ara_analysis.cpp
+++ examples/example_ara_analysis.cpp
@@ -43,16 +43,16 @@ int main(int argc, char **argv)
 		return -1;
 	}
 	
-	TFile *fpIn = new TFile(argv[2], "OLD"); //we're going to open the data file
+	TFile *fpIn = new TFile(argv[1], "OLD"); //we're going to open the data file
 	if(!fpIn){
-		std::cerr<< "Can not open the old file: " <<argv[2]<<endl;
+		std::cerr<< "Can not open the old file: " <<argv[1]<<endl;
 		return -1;
 	} //throw a warning if you can't open it
 	
 	fpIn->cd(); //go into that file
 	TTree *eventTree = (TTree*) fpIn->Get("eventTree"); //load in the event free for this file
 	if(!eventTree){
-		std::cerr<<"Can't find eventTree in file" <<argv[2]<<" with filename " <<argv[2]<<endl;
+		std::cerr<<"Can't find eventTree in file" <<argv[1]<<" with filename " <<argv[1]<<endl;
 		return -1;
 	} //throw a warning if you can't open it
 	 //set the tree address to access our raw data type
