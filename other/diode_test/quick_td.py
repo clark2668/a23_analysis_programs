ecd0f17bd3483786d4f5b7b373cb3235cc60b5a4
clark2668 <clark.2668@osu.edu>
2019-03-25 09:12:08 -0400

    Remove frivolous scan and fix error in tunnel_diode function with n_pts

--- other/diode_test/quick_td.py
+++ other/diode_test/quick_td.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt #import matplotlib
 from matplotlib.pyplot import *
 rcParams['mathtext.default'] = 'regular'
 import math
-from scipy import signal
 import scipy
+from scipy import signal
 
 td_args = {
         'down1': (-0.8, 15e-9, 2.3e-9, 0),
@@ -34,28 +34,36 @@ def td_fup(x):
 
 
 def tunnel_diode(self, dt, trace):
-        t_max = 1e-7
-        n_pts = int(t_max * dt)
-        times = np.linspace(0, t_max, n_pts + 1)
-        diode_resp = td_fdown1(times) + td_fdown2(times)
-        t_slice = times > td_args['up'][1]
-        diode_resp[t_slice] += td_fup(times[t_slice])
-        conv = scipy.signal.convolve(trace ** 2 , diode_resp, mode='full')
-        # conv multiplied by dt so that the amplitude stays constant for
-        # varying dts (determined emperically, see ARVZAskaryanSignal comments)
-        # Setting output
-        trace_after_tunnel_diode = conv / dt
-        trace_after_tunnel_diode = trace_after_tunnel_diode[:trace.shape[0]]
-        return trace_after_tunnel_diode
+	t_max = 1e-7
+	n_pts = int(t_max / dt)
+	times = np.linspace(0, t_max, n_pts + 1)
+	print times
+	diode_resp = td_fdown1(times) + td_fdown2(times)
+	t_slice = times > td_args['up'][1]
+	diode_resp[t_slice] += td_fup(times[t_slice])
+
+	fig = plt.figure(figsize=(11,8.5)) #make a figure object
+	ax = fig.add_subplot(1,1,1) #make a subplot for the limit
+	ax.plot(times,diode_resp)
+	print diode_resp
+	fig.savefig("diode_resp.png",edgecolor='none',bbox_inches="tight") #save the figure       
+
+	conv = scipy.signal.convolve(trace ** 2 , diode_resp, mode='full')
+	# conv multiplied by dt so that the amplitude stays constant for
+	# varying dts (determined emperically, see ARVZAskaryanSignal comments)
+	# Setting output
+	trace_after_tunnel_diode = conv / dt
+	trace_after_tunnel_diode = trace_after_tunnel_diode[:trace.shape[0]]
+	return trace_after_tunnel_diode
 
 
 def main():	
 	
 	times = np.linspace(0,256e-9,256)
-	
+
 	noise = np.random.normal(0,11*1e-6,256)
-	imp = signal.unit_impulse(256, 'mid')
-	b, a = signal.butter(9, 0.6)
+	imp = scipy.signal.unit_impulse(256, 'mid')
+	b, a = scipy.signal.butter(9, 0.6)
 	response = signal.lfilter(b, a, imp)
 	noisy_signal = (response*300*1e-6)+noise
 	td = tunnel_diode(noisy_signal,1e-9,noisy_signal)
@@ -66,28 +74,5 @@ def main():
 	ax2 = fig.add_subplot(2,1,2) #make a subplot for the limit
 	ax2.plot(times,td)
 	fig.savefig("test.png",edgecolor='none',bbox_inches="tight") #save the figure
-	
-	times_new = np.linspace(0,256e-9,256)
-	amp_levels = np.logspace(-3,0)
-	#amp_levels = np.array([1e-7,1e-6,1e-7,1e-5,1e-3,1e-2,1e-1,1e0,1e1])
-	max_levels = np.empty(len(amp_levels))
-	volt_squared = np.empty(len(amp_levels))
-	multiple = np.empty(len(amp_levels))
-	for i, level in enumerate(amp_levels):
-		max_levels[i] = np.max(np.abs(response*level))
-		volt_squared[i]=level**2
-		multiple[i]=max_levels[i]/volt_squared[i]
-		
-		
-	print amp_levels[2]/amp_levels[0]	
-	fig2 = plt.figure(figsize=(11,8.5)) #make a figure object
-	ax3 = fig2.add_subplot(1,1,1) #make a subplot for the limit
-	ax3.plot(amp_levels,max_levels,'o-')
-	ax3.plot(amp_levels,volt_squared,'s-')
-	#ax3.set_yscale('log')
-	ax3.set_xscale('log')
-	#ax3.axvline(x=11e-6,linestyle='--',color='red')
-	fig2.savefig("test2.png",edgecolor='none',bbox_inches="tight") #save the figure
-	
 		
 main()
