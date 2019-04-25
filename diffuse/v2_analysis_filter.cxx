078bcfed4cc50073a60038fd89327b1f871d5aa6
Brian Clark <clark.2668@osu.edu>
2019-04-24 23:56:23 -0400

    revert to old version of run number ID... will need to find something more robust later

--- diffuse/v2_analysis_filter.cxx
+++ diffuse/v2_analysis_filter.cxx
@@ -184,9 +184,7 @@ int main(int argc, char **argv)
 	Long64_t starEvery=numEntries/80;
 	if(starEvery==0) starEvery++;
 		
-	int runNum;
-	eventTree->SetBranchAddress("run",&runNum);
-	eventTree->GetEntry(0);
+	int runNum = getrunNum(argv[6]);
 	printf("Filter Run Number %d \n", runNum);
 	printf("Num entries is %d \n", numEntries);
 
