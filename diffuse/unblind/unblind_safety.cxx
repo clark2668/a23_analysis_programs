6f55bf32d2b9d4e1e7ecb0ba025d58eb14bff8fd
Brian Clark <clark.2668@osu.edu>
2019-08-26 14:56:24 -0400

    start folder for code that does the unblinding

---
+++ diffuse/unblind/unblind_safety.cxx
@@ -0,0 +1,297 @@
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////	unblind_safety.cxx
+////	unblind the safety stripes
+////
+////	Nov 2018
+////////////////////////////////////////////////////////////////////////////////
+
+//C++
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <math.h>
+#include <sys/stat.h>
+
+//ROOT Includes
+#include "TTree.h"
+#include "TFile.h"
+#include "TH2D.h"
+#include "TCanvas.h"
+#include "TStyle.h"
+#include "TF1.h"
+#include "TLegend.h"
+#include "TRandom3.h"
+#include "TChain.h"
+
+// analysis custom
+#include "tools_Cuts.h"
+#include "tools_Stats.h"
+#include "tools_CommandLine.h"
+#include "tools_outputObjects.h"
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+	
+	time_t time_now = time(0); //get the time now                                                                                                                                                                  
+	tm *time = localtime(&time_now);
+	int year_now = time -> tm_year + 1900;
+	int month_now = time -> tm_mon + 1;
+	int day_now = time -> tm_mday;
+
+	stringstream ss;
+	gStyle->SetOptStat(0);
+
+	char *plotPath(getenv("PLOT_PATH"));
+	if (plotPath == NULL) std::cout << "Warning! $PLOT_PATH is not set!" << endl;
+
+	if(argc<2){
+		cout<< "Usage\n" << argv[0] << " <1-station> <2-config> "<<endl;;
+		return -1;
+	}
+	int station = atoi(argv[1]);
+	int config = atoi(argv[2]);
+
+	if(station!=2 && station!=3){
+		printf("No good! You asked for station %d, but this code only works for stations 2 and 3 \n",station);
+		return -1;
+	}
+
+	vector<int> BadRunList=BuildBadRunList(station);
+
+	int numTotal=0;
+
+	// need to be able to make the final 2D distribution
+	double max=0.05;
+	TH2D *h2SNRvsCorr[2]; // SNR on Y axis, Corr on X axis, like in the TB
+	h2SNRvsCorr[0]=new TH2D("","V Data",100,0,max,300,0,30);
+	h2SNRvsCorr[1]=new TH2D("","H Data",100,0,max,300,0,30);
+
+	TChain dataVTree("VTree");
+	TChain dataHTree("HTree");
+	TChain dataAllTree("AllTree");
+	char the_data[500];
+	// sprintf(the_data,"/fs/project/PAS0654/ARA_DATA/A23/100pct/ValsForCuts/A%d/c%d/cutvals_drop_FiltSurface_snrbins_0_0_wfrmsvals_-1.3_-1.4_run_*.root",station,config);
+	// sprintf(the_data,"/fs/project/PAS0654/ARA_DATA/A23/10pct_verify/ValsForCuts/A%d/c%d/cutvals_drop_FiltSurface_snrbins_0_0_wfrmsvals_-1.3_-1.4_run_*.root",station,config);
+	sprintf(the_data,"/fs/project/PAS0654/ARA_DATA/A23/10pct/ValsForCuts_UsedInA2FinalOpt//A%d/c%d/cutvals_drop_FiltSurface_snrbins_0_0_wfrmsvals_-1.3_-1.4_run_*.root",station,config);
+	int result = dataVTree.Add(the_data);
+
+	dataHTree.Add(the_data);
+	dataAllTree.Add(the_data);
+	int numDataEvents = dataVTree.GetEntries();
+	// numDataEvents=100;
+
+	// do this inside brackets for scoping power and re-use of identical variable names when it comes time for simulation to happen
+	{
+
+		double corr_val[2];
+		double snr_val[2];
+		int WFRMS[2];
+		int theta_300[2];
+		int phi_300[2];
+		int theta_41[2];
+		int phi_41[2];
+
+		int Refilt[2];
+		dataVTree.SetBranchAddress("Refilt_V",&Refilt[0]);
+		dataHTree.SetBranchAddress("Refilt_H",&Refilt[1]);
+
+		dataVTree.SetBranchAddress("corr_val_V_new",&corr_val[0]);
+		dataVTree.SetBranchAddress("snr_val_V_new",&snr_val[0]);
+		dataVTree.SetBranchAddress("wfrms_val_V_new",&WFRMS[0]);
+		dataVTree.SetBranchAddress("theta_300_V_new",&theta_300[0]);
+		dataVTree.SetBranchAddress("theta_41_V_new",&theta_41[0]);
+		dataVTree.SetBranchAddress("phi_300_V_new",&phi_300[0]);
+		dataVTree.SetBranchAddress("phi_41_V_new",&phi_41[0]);
+
+		dataHTree.SetBranchAddress("corr_val_H_new",&corr_val[1]);
+		dataHTree.SetBranchAddress("snr_val_H_new",&snr_val[1]);
+		dataHTree.SetBranchAddress("wfrms_val_H_new",&WFRMS[1]);
+		dataHTree.SetBranchAddress("theta_300_H_new",&theta_300[1]);
+		dataHTree.SetBranchAddress("theta_41_H_new",&theta_41[1]);
+		dataHTree.SetBranchAddress("phi_300_H_new",&phi_300[1]);
+		dataHTree.SetBranchAddress("phi_41_H_new",&phi_41[1]);
+
+		int isCal;
+		int isSoft;
+		int isShort;
+		int isCW;
+		int isNewBox;
+
+		dataAllTree.SetBranchAddress("cal",&isCal);
+		dataAllTree.SetBranchAddress("soft",&isSoft);
+		dataAllTree.SetBranchAddress("short",&isShort);
+		dataAllTree.SetBranchAddress("CW",&isCW);
+		dataAllTree.SetBranchAddress("box",&isNewBox);
+
+		int isSurf[2]; // a surface event after filtering?
+		int isSurfEvent_top[2]; // a top event?
+
+		dataAllTree.SetBranchAddress("surf_V_new",&isSurf[0]);
+		dataAllTree.SetBranchAddress("surf_H_new",&isSurf[1]);
+
+		dataAllTree.SetBranchAddress("surf_top_V",&isSurfEvent_top[0]);
+		dataAllTree.SetBranchAddress("surf_top_H",&isSurfEvent_top[1]);
+
+		int isBadEvent;
+		double weight;
+		int unixTime;
+		int isFirstFiveEvent;
+		int hasBadSpareChanIssue;
+		int hasBadSpareChanIssue2;
+		int runNum;
+
+		dataAllTree.SetBranchAddress("bad",&isBadEvent);
+		dataAllTree.SetBranchAddress("weight",&weight);
+		dataAllTree.SetBranchAddress("unixTime",&unixTime);
+		dataAllTree.SetBranchAddress("isFirstFiveEvent",&isFirstFiveEvent);
+		dataAllTree.SetBranchAddress("hasBadSpareChanIssue",&hasBadSpareChanIssue);
+		dataAllTree.SetBranchAddress("hasBadSpareChanIssue2",&hasBadSpareChanIssue2);
+		dataAllTree.SetBranchAddress("runNum",&runNum);
+
+		double frac_of_power_notched_V[8];
+		double frac_of_power_notched_H[8];
+
+		stringstream ss;
+		for(int i=0; i<8; i++){
+			ss.str("");
+			ss<<"PowerNotch_Chan"<<i;
+			dataVTree.SetBranchAddress(ss.str().c_str(),&frac_of_power_notched_V[i]);
+		}
+		for(int i=8; i<16; i++){
+			ss.str("");
+			ss<<"PowerNotch_Chan"<<i;
+			dataHTree.SetBranchAddress(ss.str().c_str(),&frac_of_power_notched_H[i-8]);
+		}
+
+		int numEntries = dataVTree.GetEntries();
+		numTotal+=numEntries;
+
+		dataAllTree.GetEvent(0);
+		int currentRunNum = runNum;
+		bool isThisABadRun = isBadRun(station,runNum,BadRunList);
+
+		for(int event=0; event<numDataEvents; event++){
+			dataVTree.GetEvent(event);
+			dataHTree.GetEvent(event);
+			dataAllTree.GetEvent(event);
+			if(runNum!=currentRunNum){
+				currentRunNum=runNum;
+				isThisABadRun = isBadRun(station,runNum, BadRunList);
+				if(isThisABadRun)
+					printf(RED"*"RESET);
+					// printf("     Yup, run %d is bad \n",runNum);
+				else
+					printf(GREEN"*"RESET);
+
+			}
+			if( isSoft || isBadEvent || hasBadSpareChanIssue || hasBadSpareChanIssue2 || isFirstFiveEvent || isShort || isCal || isThisABadRun){
+				continue;
+			}
+			if(isBadLivetime(station,unixTime)){
+				continue;
+			}
+			for(int pol=0; pol<2; pol++){
+				if(!WFRMS[pol] && !isNewBox && !isSurf[0] && !isSurf[1] && !isSurfEvent_top[pol]){
+					bool failsCWPowerCut=false;
+					if(Refilt[pol] && !WFRMS[pol]){
+						vector<double> frac;
+						for(int i=0; i<8; i++){
+							if(pol==0) frac.push_back(frac_of_power_notched_V[i]);
+							else if(pol==1) frac.push_back(frac_of_power_notched_H[i]);
+						} 
+						sort(frac.begin(), frac.end(), std::greater<double>());
+						if(frac[2]>0.06){
+							failsCWPowerCut=true;
+						}
+					} //refiltered?
+					if(!failsCWPowerCut){
+
+						// now, we only plot if the event is in the "safety cut" region
+						if( corr_val[pol] <=0.005 || snr_val[pol]<=4 ){
+							h2SNRvsCorr[pol]->Fill(corr_val[pol],snr_val[pol],weight);
+						}
+
+					}// not failing CW power cut?
+				}// passes rest of analysis (not WFRMS, box, surface)
+			}// loop over polarizations
+		}// loop over events
+	}
+	std::cout<<endl;
+
+	vector<TGraph*> cut_lines;
+	for(int pol=0; pol<2; pol++){
+		vector <double> x_vals_for_line;
+		vector <double> y_vals_for_line;
+		double rcut_slope;
+		double rcut_intercept;
+		getRCutValues(station, config, pol, rcut_slope, rcut_intercept);
+		for(double x=0; x<0.020; x+=0.00001){
+			double y_val = (rcut_slope * x ) + rcut_intercept;
+			x_vals_for_line.push_back(x);
+			y_vals_for_line.push_back(y_val);
+		}
+		cut_lines.push_back(new TGraph(x_vals_for_line.size(), &x_vals_for_line[0], &y_vals_for_line[0]));
+	}
+
+	// now to actually draw the safety cut lines
+	double vert_cuts[2] = {0.005, 0.005};
+	vector<TGraph*> vert_lines;
+	for(int pol=0; pol<2; pol++){
+		vector <double> x_vals_for_line;
+		vector <double> y_vals_for_line;
+		for(double y=0; y<30; y+=1.){
+			y_vals_for_line.push_back(y);
+			x_vals_for_line.push_back(vert_cuts[pol]);
+		}
+		vert_lines.push_back(new TGraph(x_vals_for_line.size(), &x_vals_for_line[0], &y_vals_for_line[0]));
+	}
+
+	double horz_cuts[2] = {4., 4.};
+	vector<TGraph*> horz_lines;
+	for(int pol=0; pol<2; pol++){
+		vector <double> x_vals_for_line;
+		vector <double> y_vals_for_line;
+		for(double x=0; x<0.05; x+=0.01){
+			y_vals_for_line.push_back(horz_cuts[pol]);
+			x_vals_for_line.push_back(x);
+		}
+		horz_lines.push_back(new TGraph(x_vals_for_line.size(), &x_vals_for_line[0], &y_vals_for_line[0]));
+	}
+
+	vector<TGraph*> horz_lines2;
+	for(int pol=0; pol<2; pol++){
+		vector <double> x_vals_for_line;
+		vector <double> y_vals_for_line;
+		for(double x=0; x<0.05; x+=0.01){
+			y_vals_for_line.push_back(horz_cuts[pol]);
+			x_vals_for_line.push_back(x);
+		}
+		horz_lines2.push_back(new TGraph(x_vals_for_line.size(), &x_vals_for_line[0], &y_vals_for_line[0]));
+	}	
+
+	// okay, now save out the 2D histogram
+	TCanvas *cSNRvsCorr = new TCanvas("","",2.1*850, 850);
+	cSNRvsCorr->Divide(2,1);
+	for(int pol=0; pol<2; pol++){
+		cSNRvsCorr->cd(pol+1);
+		h2SNRvsCorr[pol]->Draw("colz");
+		h2SNRvsCorr[pol]->GetYaxis()->SetTitle("3rd Highest VPeak/RMS");
+		h2SNRvsCorr[pol]->GetXaxis()->SetTitle("Peak Corr");
+		gPad->SetLogz();
+		cut_lines[pol]->Draw("same");
+		cut_lines[pol]->SetLineColor(kRed);
+		vert_lines[pol]->Draw("same");
+		vert_lines[pol]->SetLineColor(kBlue);
+		horz_lines[pol]->Draw("same");
+		horz_lines[pol]->SetLineColor(kBlue);
+	}
+	char title[300];
+	sprintf(title, "%s/unblind/A%d_config%d_%dEvents_UnblindSides_SNRvsCorr.png",plotPath,station,config,numTotal);
+	cSNRvsCorr->SaveAs(title);
+	delete cSNRvsCorr;
+
+}
