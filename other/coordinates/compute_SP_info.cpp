4522ac5eba92d6c4185d7d48c8b1ca5f6094ff3e
Brian Clark <clark.2668@osu.edu>
2019-10-11 10:22:06 -0400

    name some variables more appropriately, and add the computation of the phi of the other station in the station local coordinates

--- other/coordinates/compute_SP_info.cpp
+++ other/coordinates/compute_SP_info.cpp
@@ -28,21 +28,19 @@ int main(int argc, char **argv)
 	/*
 		The South Pole
 	*/
-
 	AraGeomTool *araGeom = AraGeomTool::Instance();
 	Double_t *SP_ARACoord = araGeom->getSouthPole2010_11();
 	// printf("SP_ARACoord is %.2f, %.2f, %.2f \n", SP_ARACoord[0],SP_ARACoord[1],SP_ARACoord[2]);
 
-	Double_t SP_A2Coord[3];
-	araGeom->convertArrayToStationCoords(station, SP_ARACoord, SP_A2Coord );
-	// printf("SP_A2Coord is %.2f, %.2f, %.2f \n", SP_A2Coord[0],SP_A2Coord[1],SP_A2Coord[2]);
-	double phi_SP_A2Coord = TMath::ATan2(SP_A2Coord[1],SP_A2Coord[0])*TMath::RadToDeg();
-	printf("Phi of SP is %.2f \n", phi_SP_A2Coord);
+	Double_t SP_StationCoord[3];
+	araGeom->convertArrayToStationCoords(station, SP_ARACoord, SP_StationCoord );
+	// printf("SP_StationCoord is %.2f, %.2f, %.2f \n", SP_StationCoord[0],SP_StationCoord[1],SP_StationCoord[2]);
+	double phi_SP_StationCoord = TMath::ATan2(SP_StationCoord[1],SP_StationCoord[0])*TMath::RadToDeg();
+	printf("Phi of SP is %.2f \n", phi_SP_StationCoord);
 
 	/*
 		The ICL
 	*/
-
 	Double_t *ICL_ARACoord = araGeom->getICLCorner(0);
 	for(int i=1; i<=3; i++){
 		Double_t *other_corner = araGeom->getICLCorner(i);
@@ -55,25 +53,49 @@ int main(int argc, char **argv)
 	ICL_ARACoord[2]/=4.;
 	// printf("ICL_ARACoord is %.2f, %.2f, %.2f \n", ICL_ARACoord[0],ICL_ARACoord[1],ICL_ARACoord[2]);
 
-	Double_t ICL_A2Coord[3];
-	araGeom->convertArrayToStationCoords(station, ICL_ARACoord, ICL_A2Coord );
-	// printf("ICL_A2Coord is %.2f, %.2f, %.2f \n", ICL_A2Coord[0],ICL_A2Coord[1],ICL_A2Coord[2]);
-	double phi_ICL_A2Coord = TMath::ATan2(ICL_A2Coord[1],ICL_A2Coord[0])*TMath::RadToDeg();
-	printf("Phi of ICL is %.2f \n", phi_ICL_A2Coord);
+	Double_t ICL_StationCoord[3];
+	araGeom->convertArrayToStationCoords(station, ICL_ARACoord, ICL_StationCoord );
+	// printf("ICL_StationCoord is %.2f, %.2f, %.2f \n", ICL_StationCoord[0],ICL_StationCoord[1],ICL_StationCoord[2]);
+	double phi_ICL_StationCoord = TMath::ATan2(ICL_StationCoord[1],ICL_StationCoord[0])*TMath::RadToDeg();
+	printf("Phi of ICL is %.2f \n", phi_ICL_StationCoord);
 
 	
 	/*
 		WT3
 	*/
-
 	Double_t *WT3_ARACoord = araGeom->getWindTurbine(3);
 	// printf("WT3_ARACoord is %.2f, %.2f, %.2f \n", WT3_ARACoord[0],WT3_ARACoord[1],WT3_ARACoord[2]);
 
-	Double_t WT3_A2Coord[3];
-	araGeom->convertArrayToStationCoords(station, WT3_ARACoord, WT3_A2Coord );
-	// printf("WT3_A2Coord is %.2f, %.2f, %.2f \n", WT3_A2Coord[0],WT3_A2Coord[1],WT3_A2Coord[2]);
-	double phi_WT3_A2Coord = TMath::ATan2(WT3_A2Coord[1],WT3_A2Coord[0])*TMath::RadToDeg();
-	printf("Phi of WT3 is %.2f \n", phi_WT3_A2Coord);
+	Double_t WT3_StationCoord[3];
+	araGeom->convertArrayToStationCoords(station, WT3_ARACoord, WT3_StationCoord );
+	// printf("WT3_StationCoord is %.2f, %.2f, %.2f \n", WT3_StationCoord[0],WT3_StationCoord[1],WT3_StationCoord[2]);
+	double phi_WT3_StationCoord = TMath::ATan2(WT3_StationCoord[1],WT3_StationCoord[0])*TMath::RadToDeg();
+	printf("Phi of WT3 is %.2f \n", phi_WT3_StationCoord);
+
+
+	/*
+		Phi of the other station
+		That is, if we ask for station 2, what is the phi of station 3
+		and vice versa
+	*/
+
+	int other_station;
+	if(station==2) other_station=3;
+	else if(station==3) other_station=2;
+	printf("The other station is %d \n", other_station);
+
+	// the coordinates of the other station in array centric coordinates
+	TVector3 tv3OtherStation_ARACoords = araGeom->getStationVector(other_station);
+	Double_t OtherStation_ARACoords[3];
+	for(int i=0; i<3; i++){
+		OtherStation_ARACoords[i] = tv3OtherStation_ARACoords[i];
+	}
+	// printf("Coordinates of other station %d: %.2f, %.2f, %.2f \n", other_station, OtherStation_ARACoords[0], OtherStation_ARACoords[1], OtherStation_ARACoords[2]);
+
+	Double_t OtherStation_StationCoords[3]; //what is the position of the other station in the coordinates of the local station
+	araGeom->convertArrayToStationCoords(station, OtherStation_ARACoords, OtherStation_StationCoords);
+	double phi_OtherStation_StationCoord = TMath::ATan2(OtherStation_StationCoords[1],OtherStation_StationCoords[0])*TMath::RadToDeg();
+	printf("Phi of Other Station %d is %.2f \n", other_station, phi_OtherStation_StationCoord);
 
 
 	/*
